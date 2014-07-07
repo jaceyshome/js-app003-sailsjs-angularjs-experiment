621dd6267aca5cd79d0da97c436e00ebb412d881
Jacob Wang <jacob.wang@change2.com.au>
2014-07-07 18:34:02 +1000

    restrusture

--- config/routes.js
+++ config/routes.js
@@ -33,7 +33,7 @@ module.exports.routes = {
   // 
   // (This would also work if you had a file at: `/views/home.ejs`)
   '/': {
-    view: 'app/app'
+    view: 'app'
   },
 //  '/rest-examples': {
 //    view: 'main/rest'
