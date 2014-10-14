7fd73992216b9c99708b967203b23cd1287fe170
jake-Ubuntu <jaceyshome>
2014-10-14 20:39:22 +1100

    protractor html report page

--- protractorConf.js
+++ protractorConf.js
@@ -1,7 +1,13 @@
+var HtmlReporter = require('protractor-html-screenshot-reporter');
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   baseUrl: 'http://localhost:1337',
   capabilities: {
     'browserName': 'chrome'
+  },
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(new HtmlReporter({
+      baseDirectory: '.tmp/screenshots'
+    }));
   }
 };
