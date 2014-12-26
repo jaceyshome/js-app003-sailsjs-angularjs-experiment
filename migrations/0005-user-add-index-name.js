f723e8c97373b8ae202cc975652d2f6a5bf1661b
Jacob Wang <jacob.wang@change2.com.au>
2014-12-27 00:50:14 +1100

    clean up migration script

--- migrations/0005-user-add-index-name.js
+++ migrations/0005-user-add-index-name.js
@@ -2,13 +2,15 @@
 var mongodb = require('mongodb');
 
 exports.up = function(db, next){
-  var user = mongodb.Collection(db, 'user');
-  user.ensureIndex( { "name": 1 }, { unique: true }, function(err, indexName) {
-    if(err){
-      return err
-    }
-    next();
-  });
+  mongodb.Collection(db, 'user').ensureIndex(
+    { "name": 1 },
+    { unique: true },
+    function(err, indexName) {
+      if(err){
+        return err;
+      }
+      next();
+    });
 };
 
 exports.down = function(db, next){
